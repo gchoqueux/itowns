17661f4be3506dbd907fc6f1c610317055d74d81
Gerald Choqueux <gerald.choqueux@ign.fr>
2018-01-25 09:47:41 +0100

    stencil remaster

--- utils/debug/Debug.js
+++ utils/debug/Debug.js
@@ -1,3 +1,4 @@
+import * as THREE from 'three';
 import Coordinates from '../../src/Core/Geographic/Coordinates';
 import ThreeStatsChart from './charts/ThreeStatsChart';
 
@@ -28,6 +29,7 @@ function Debug(view, datDebugTool, chartDivContainer) {
     this.charts.push(new ThreeStatsChart('three-info', view.mainLoop.gfxEngine.renderer));
 
     const charts = this.charts;
+    const tileLayer = view.tileLayer || view.wgs84TileLayer || view.baseLayer;
 
     function debugChartUpdate(updateDuration) {
         const displayed = chartDivContainer.style.display != 'none';
@@ -40,6 +42,8 @@ function Debug(view, datDebugTool, chartDivContainer) {
     const state = {
         displayCharts: false,
         eventsDebug: false,
+        debugCameraWindow: false,
+        freeze: false,
     };
 
     // charts
@@ -48,6 +52,22 @@ function Debug(view, datDebugTool, chartDivContainer) {
             chartDivContainer.style.display = 'flex';
         } else {
             chartDivContainer.style.display = 'none';
+        }
+    });
+
+    gui.add(state, 'debugCameraWindow').name('debug Camera').onChange(() => {
+        view.notifyChange(true);
+    });
+
+
+    let update = tileLayer.update;
+    gui.add(state, 'freeze').name('freeze update').onChange((newValue) => {
+        if (newValue) {
+            update = tileLayer.update;
+            tileLayer.update = () => {};
+        } else {
+            tileLayer.update = update;
+            view.notifyChange(true);
         }
     });
 
@@ -95,6 +115,113 @@ function Debug(view, datDebugTool, chartDivContainer) {
         // debug graphs update
         debugChartUpdate(duration);
     };
+
+    // Camera debug
+    const g = view.mainLoop.gfxEngine;
+    const r = g.renderer;
+    const gl = r.context;
+    // let fogDistance = view.fogDistance;
+
+    const sceneStencil = new THREE.Scene();
+
+    var loader = new THREE.FontLoader();
+
+    loader.load('https://raw.githubusercontent.com/mrdoob/three.js/dev/examples/fonts/helvetiker_regular.typeface.json', (font) => {
+
+        // THREE.FrontSide
+        // THREE.BackSide
+        // THREE.DoubleSide
+
+        // const geometry2 = new THREE.CylinderGeometry(1000000, 1000000, 100000000, 32);
+        // const material2 = new THREE.MeshBasicMaterial({ color: 0xffff00 });
+        // material2.side = THREE.DoubleSide;
+        // const cyl = new THREE.Mesh(geometry2, material2);
+        // cyl.position.set(5000000, 0, 0);
+
+        // cyl.rotateZ(Math.PI * 0.5);
+        // cyl.updateMatrix();
+        // cyl.updateMatrixWorld(true);
+        // cyl.matrixAutoUpdate = false;
+
+        // sceneStencil.add(cyl);
+        // view.scene.add(cyl);
+
+
+        // const geometry = new THREE.SphereGeometry(3000000, 128, 128);
+        // const material = new THREE.MeshBasicMaterial({ color: 0xffff00 });
+        // const text = new THREE.Mesh(geometry, material);
+        // text.position.set(5000000, 0, 0);
+        // const scale = 1;
+        // text.scale.set(scale, scale, scale);
+        // text.updateMatrix();
+        // text.updateMatrixWorld(true);
+        // text.matrixAutoUpdate = false;
+
+        const geometry = new THREE.TextGeometry('ITOWNS', {
+            font,
+            size: 80,
+            height: 1000,
+            curveSegments: 12,
+            bevelEnabled: false,
+        });
+        const material = new THREE.MeshBasicMaterial({ color: 0xffff00 });
+        material.side = THREE.BackSide;
+        const text = new THREE.Mesh(geometry, material);
+        text.position.set(5000000, -1800000, 0);
+        text.rotateY(Math.PI * 0.5);
+        text.rotateZ(Math.PI * 0.5);
+        const scale = 10000;
+        text.scale.set(scale, scale, scale);
+        text.updateMatrix();
+        text.updateMatrixWorld(true);
+        text.matrixAutoUpdate = false;
+
+        sceneStencil.add(text);
+        // view.scene.add(text);
+    });
+
+
+    view.atmosphere.visible = false;
+    tileLayer.disableSkirt = true;
+
+    view.render = function render() {
+        r.clear();
+
+        // enable stencil test
+        r.state.buffers.stencil.setTest(true);
+        r.setViewport(0, 0, g.width, g.height);
+
+        r.setFaceCulling(THREE.CullFaceBack);
+        r.state.buffers.stencil.setFunc(gl.ALWAYS, 1, 0xff);
+        r.state.buffers.stencil.setOp(gl.KEEP, gl.KEEP, gl.INCR);
+        r.render(view.scene, view.camera.camera3D);
+
+        r.setFaceCulling(THREE.CullFaceFront);
+        r.state.buffers.stencil.setFunc(gl.EQUAL, 1, 0xff);
+        r.state.buffers.stencil.setOp(gl.KEEP, gl.KEEP, gl.DECR);
+        r.render(sceneStencil, view.camera.camera3D);
+
+        r.clear(true, true, false);
+        r.setFaceCulling(THREE.CullFaceBack);
+        r.state.buffers.stencil.setFunc(gl.EQUAL, 1, 0xff);
+        r.state.buffers.stencil.setOp(gl.KEEP, gl.KEEP, gl.KEEP);
+        r.render(sceneStencil, view.camera.camera3D);
+
+        r.state.buffers.stencil.setTest(false);
+        r.setFaceCulling(THREE.CullFaceBack);
+        r.render(view.scene, view.camera.camera3D);
+    };
+
+
+    const composer = new THREE.EffectComposer(r);
+    composer.addPass(new THREE.RenderPass(view.scene, view.camera.camera3D));
+
+    const effect = new THREE.ShaderPass(THREE.CopyShader);
+    effect.renderToScreen = true;
+    composer.addPass(effect);
+
+    r.setSize(g.width, g.height);
+    composer.setSize(g.width, g.height);
 }
 
 
