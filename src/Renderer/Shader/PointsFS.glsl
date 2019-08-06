9c3fbcf9df2c141dedde5b67d7485f7eea32a7e7
gchoqueux <gerald.choqueux@ign.fr>
2019-08-06 17:48:16 +0200

    feature(point): option to link OrientedImageMaterial to PointsMaterial.

--- src/Renderer/Shader/PointsFS.glsl
+++ src/Renderer/Shader/PointsFS.glsl
@@ -1,17 +1,36 @@
-precision highp float;
-precision highp int;
-
+#include <itowns/precision_qualifier>
 #include <logdepthbuf_pars_fragment>
+#if defined(USE_TEXTURES_PROJECTIVE)
+#include <itowns/projective_texturing_pars_fragment>
+#endif
 
 varying vec4 vColor;
-
+uniform bool pickingMode;
 void main() {
+    #include <logdepthbuf_fragment>
     // circular point rendering
     if(length(gl_PointCoord - 0.5) > 0.5){
         discard;
     }
 
-    gl_FragColor = vColor;
+#if defined(USE_TEXTURES_PROJECTIVE)
+    if (!pickingMode) {
+        vec4 color = vColor;
+        vec4 aColor;
 
-    #include <logdepthbuf_fragment>
+        #pragma unroll_loop
+        for (int i = 0; i < ORIENTED_IMAGES_COUNT; i++) {
+            aColor = projectiveTextureColor(projectiveTextureCoords[ ORIENTED_IMAGES_COUNT - 1 - i ], projectiveTextureDistortion[ ORIENTED_IMAGES_COUNT - 1 - i ], projectiveTexture[ ORIENTED_IMAGES_COUNT - 1 - i ], mask[ORIENTED_IMAGES_COUNT - 1 - i]);
+
+            color = aColor.a == 1.0 ? aColor : vec4(mix(color.rgb, aColor.rgb, aColor.a), 1.0);
+        }
+
+        gl_FragColor = color;
+    } else {
+        gl_FragColor = vColor;
+    }
+    gl_FragColor.a *= opacity;
+#else
+    gl_FragColor = vColor;
+#endif
 }
