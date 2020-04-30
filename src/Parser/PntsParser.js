b2c00c77d20433634e161045de6892029da6592b
Gérald choqueux <gerald.choqueux@ign.fr>
2020-04-30 14:27:49 +0200

    feature(3dtiles): add suppport to quantized position on points clouds 3d tiles.

--- src/Parser/PntsParser.js
+++ src/Parser/PntsParser.js
@@ -70,6 +70,7 @@ function parseFeatureBinary(array, byteOffset, FTJSONLength) {
     // Init geometry
     const geometry = new THREE.BufferGeometry();
 
+
     // init Array feature binary
     const subArrayJson = utf8Decoder.decode(new Uint8Array(array, byteOffset, FTJSONLength));
     const parseJSON = JSON.parse(subArrayJson);
@@ -87,29 +88,46 @@ function parseFeatureBinary(array, byteOffset, FTJSONLength) {
         const colorArray = new Uint8Array(array, byteOffsetCol, lengthFeature * 3);
         geometry.setAttribute('color', new THREE.BufferAttribute(colorArray, 3, true));
     }
+    // eslint-disable-next-line no-debugger
+    // debugger;
     if (parseJSON.POSITION_QUANTIZED) {
-        throw new Error('For pnts loader, POSITION_QUANTIZED: not yet managed');
+        const byteOffsetPos = (parseJSON.POSITION_QUANTIZED.byteOffset + subArrayJson.length + byteOffset);
+        const qpositionArray = new Uint16Array(array, byteOffsetPos, lengthFeature * 3);
+        // console.log('positionArray', qpositionArray);
+        const positionArray = new Float32Array(qpositionArray.length);
+        for (var i = 0; i < positionArray.length; i += 3) {
+            positionArray[i] = qpositionArray[i] *  parseJSON.QUANTIZED_VOLUME_SCALE[0] / 65535.0 + parseJSON.QUANTIZED_VOLUME_OFFSET[0];
+            positionArray[i + 1] = qpositionArray[i + 1] *  parseJSON.QUANTIZED_VOLUME_SCALE[1] / 65535.0 + parseJSON.QUANTIZED_VOLUME_OFFSET[1];
+            positionArray[i + 2] = qpositionArray[i + 2] *  parseJSON.QUANTIZED_VOLUME_SCALE[2] / 65535.0 + parseJSON.QUANTIZED_VOLUME_OFFSET[2];
+        }
+
+        // console.log('positionArray', positionArray);
+        // qpositionArray.forEach()
+
+        geometry.setAttribute('position', new THREE.BufferAttribute(positionArray, 3));
+        // throw new Error('For pnts loader, POSITION_QUANTIZED: not yet managed');
     }
-    if (parseJSON.RGBA) {
-        throw new Error('For pnts loader, RGBA: not yet managed');
-    }
-    if (parseJSON.RGB565) {
-        throw new Error('For pnts loader, RGB565: not yet managed');
-    }
-    if (parseJSON.NORMAL) {
-        throw new Error('For pnts loader, NORMAL: not yet managed');
-    }
-    if (parseJSON.NORMAL_OCT16P) {
-        throw new Error('For pnts loader, NORMAL_OCT16P: not yet managed');
-    }
-    if (parseJSON.BATCH_ID) {
-        throw new Error('For pnts loader, BATCH_ID: not yet managed');
-    }
+    // if (parseJSON.RGBA) {
+    //     throw new Error('For pnts loader, RGBA: not yet managed');
+    // }
+    // if (parseJSON.RGB565) {
+    //     throw new Error('For pnts loader, RGB565: not yet managed');
+    // }
+    // if (parseJSON.NORMAL) {
+    //     throw new Error('For pnts loader, NORMAL: not yet managed');
+    // }
+    // if (parseJSON.NORMAL_OCT16P) {
+    //     throw new Error('For pnts loader, NORMAL_OCT16P: not yet managed');
+    // }
+    // if (parseJSON.BATCH_ID) {
+    //     throw new Error('For pnts loader, BATCH_ID: not yet managed');
+    // }
 
     // Add RTC feature
     const offset = parseJSON.RTC_CENTER ?
         new THREE.Vector3().fromArray(parseJSON.RTC_CENTER) : undefined;
-
+        // eslint-disable-next-line no-debugger
+        // debugger;
     return {
         geometry,
         offset,
