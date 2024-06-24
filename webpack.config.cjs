2f15608d6b99a6a0dcbe7978a000599b393d6b0d
gchoqueux <gerald.choqueux@ign.fr>
2024-06-24 11:01:45 +0200

    feature(VR): add rig support

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -89,6 +89,7 @@ module.exports = () => {
             }),
         ],
         devServer: {
+            server: 'https',
             devMiddleware: {
                 publicPath: '/dist/',
             },
