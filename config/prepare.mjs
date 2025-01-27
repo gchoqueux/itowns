bec33a4b0cc965d631d71cc3cd44276c8cbc552c
gchoqueux <gerald.choqueux@ign.fr>
2025-01-27 16:40:04 +0100

    chore: rool back copy examples three js

--- config/prepare.mjs
+++ config/prepare.mjs
@@ -1,6 +1,7 @@
 import chalk from 'chalk';
 import { execSync } from 'child_process';
-
+import copyfiles from 'copyfiles';
+import threeExamples from './threeExamples.mjs';
 
 
 // Verify Puppeteer configuration
@@ -30,3 +31,10 @@ const npmVersion = execSync('npm --version', {
 if (npmVersion) {
     console.log(chalk.green('Npm version :', npmVersion), '\n');
 }
+
+// Copy THREE Examples Files
+const paths = threeExamples.filesExamples.map(f => f.replace('./', './node_modules/three/examples/jsm/'));
+
+paths.push(threeExamples.patchedPath);
+
+copyfiles(paths, { up: 4 }, () => {});
