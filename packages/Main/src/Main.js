2ec58b521a26edc75697eaa2de826ffda8fb7020
gchoqueux <gerald.choqueux@ign.fr>
2025-01-29 17:24:56 +0100

    chore: to remove

--- packages/Main/src/Main.js
+++ packages/Main/src/Main.js
@@ -4,7 +4,7 @@ const conf = {
 
 export const REVISION = conf.version;
 
-// Geographic tools 22
+// Geographic tools
 export { Coordinates, Extent, CRS, Ellipsoid, ellipsoidSizes, OrientationUtils } from '@itowns/geographic';
 export { default as GeoidGrid } from 'Core/Geographic/GeoidGrid';
 
